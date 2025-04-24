c473f14fb48f8dae8a579a543c84f63b8ab7adf7
NazimAhmadDev <nazimahmedchd@gmail.com>
2025-04-24 13:42:24 +0530

    [Nazim] add : printing hello world backend completed

---
+++ src/main/java/com/example/helloWorld_backend/HelloWorldBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.helloWorld_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloWorldBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloWorld_backend/HelloWorldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloWorld_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
